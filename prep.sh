4dcff892906031b4555aa2e8495d6d7be5b23938
Ubuntu <chui@teyc-b2b-1.0zlmw3jpm2qexiijdzaomfcp2b.px.internal.cloudapp.net>
2016-09-15 13:38:42 +0000

    Download web.py

--- prep.sh
+++ prep.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 apt-get install letsencrypt
-letsencrypt certonly -d readify-b2b.australiaeast.cloudapp.azure.com
+letsencrypt certonly -d readify-linux.australiaeast.cloudapp.azure.com
 
 apt-get install daemon
 
 # enable challenge password on /etc/ssh/ssh_config
 sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
+
+if [ ! -f web.py-0.38.tar.gz ]; then
+   WEBPY_TGZ=web.py-0.38.tar.gz
+   wget http://webpy.org/static/$WEBPY_TGZ
+   tar zxf $WEBPY_TGZ
+   ln -s web.py-0.38/web web.py
+fi
 
 python app.py 443
 
