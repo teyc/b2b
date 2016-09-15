7c7d4dc229c091babbc1008195c7882ffef93f71
Chui Tey <chui.tey@readify.net>
2016-09-15 12:24:30 +0000

    Add more comments when user added

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from web.wsgiserver import CherryPyWSGIServer
 cert_path = '/etc/letsencrypt/live/readify-b2b.australiaeast.cloudapp.azure.com'
 server_name = 'readify-b2b.australiaeast.cloudapp.azure.com'
  
+# curl can't work with cert.pem?
 CherryPyWSGIServer.ssl_certificate = '%s/cert.pem' % cert_path
 CherryPyWSGIServer.ssl_certificate_chain = '%s/fullchain.pem' % cert_path
 CherryPyWSGIServer.ssl_private_key = '%s/privkey.pem' % cert_path
@@ -35,10 +36,21 @@ class adduser:
      return """
 Your username is %s
 password is %s
+You can now connect to a secure shell using
 $ ssh %s@%s
+$ 
+$ # and set up passwordless login by putting your keys in ~/.ssh/authorized_keys
 $ mkdir ~/.ssh
 $ logout
-$ scp work_rsa.pub %s@readify-b2b.australiaeast.cloudapp.azure.com:.ssh/authorized_keys
+$ #
+$ # generate key pair on your local machine
+$ #
+$ ssh-keygen -f work_rsa
+$
+$ # copy the public key over a secure connection and put it in ~/.ssh/authorized_keys
+$ scp work_rsa.pub %s@readify-b2b.australiaeast.cloudapp.azure.com:.ssh/authorized_key
+$
+$ # you can now log in using key file rather than password
 $ ssh -i work_rsa %s@readify-b2b.australiaeast.cloudapp.azure.com
 """ % (username, password, username, server_name, username, username)
        
